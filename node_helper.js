3815c52795d2fa4294ab8d6f93666ca799192705
Andriy Katkov <andriy.katkov@gmail.com>
2020-12-20 18:22:01 -0800

    remove unused imports

--- node_helper.js
+++ node_helper.js
@@ -1,9 +1,6 @@
 
 var NodeHelper = require('node_helper');
 var request = require('request');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const {Builder, By, Key, until} = require('selenium-webdriver');
 
 module.exports = NodeHelper.create({
 
@@ -49,18 +46,6 @@ module.exports = NodeHelper.create({
                 'homes': homes
             });
         });
-
-
-        // (async function example() {
-        //     let driver = await new Builder().forBrowser('chrome').build();
-        //     try {
-        //         await driver.get('http://www.google.com/ncr');
-        //         await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-        //         await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-        //     } finally {
-        //         await driver.quit();
-        //     }
-        // })();
     },
 
 
